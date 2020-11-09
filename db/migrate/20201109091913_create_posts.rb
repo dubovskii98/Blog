19b0767aec56a618507805beb7087021138af13f
dubovskii98 <dubovskii98@gmail.com>
2020-11-09 11:31:25 +0200

    add posts and categories

--- db/migrate/20201109091913_create_posts.rb
+++ db/migrate/20201109091913_create_posts.rb
@@ -2,8 +2,8 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :url
-      t.string :category_id
+      t.string :url, default: "shared url"
+      t.integer :category_id
 
       t.timestamps
     end
