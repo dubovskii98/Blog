7da28bd380b328dd174cc4eaa2b4c0554e2fef1a
dubovskii98 <dubovskii98@gmail.com>
2020-11-09 14:35:59 +0200

    add default title for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :category
+  accepts_nested_attributes_for :category
+  after_initialize :set_defaults, unless: :persisted?
+
+  def set_defaults
+    self.title  ||= ('Post ' + (Post.count(:id)+1).to_s)
+  end
 end
